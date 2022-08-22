f5ecfafdabd7b946378f5d736fb1a4d01032ba8d
LeVi <56607973+LeViCantCode@users.noreply.github.com>
2022-08-22 06:15:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+const mobileNav = document.querySelector(".nav_links");
+navToggle.addEventListener('click', async function() {
+    links.classList.toggle("show_nav");
+})
+mobileNav.addEventListener('click', async function () {
+	
+													 
+ })
+}
